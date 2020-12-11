48295838e2810e9df37d21835aa70e893997f124
sam <liulushan@vipsing.com>
2020-12-11 16:40:22 +0800

    Initial commit

---
+++ code-generator/src/main/java/com/sam/mybatis/CodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.sam.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeGeneratorApplication.class, args);
+	}
+
+}
